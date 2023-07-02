9a5aefad2b27c5a416a99b0b04fcc7c5aef5992f
Chinyere <chinyereunamba17@gmail.com>
2023-07-02 14:37:23 +0100

    finished

--- README.md
+++ README.md
@@ -10,11 +10,8 @@ This is a solution to the [Calculator app challenge on Frontend Mentor](https://
   - [Links](#links)
 - [My process](#my-process)
   - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
   - [Useful resources](#useful-resources)
 - [Author](#author)
-
-**Note: Delete this note and update the table of contents based on what sections you keep.**
 
 ## Overview
 
@@ -44,26 +41,6 @@ Users should be able to:
 - CSS custom properties
 - Flexbox
 - CSS Grid
-### What I learned
-
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
-
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
-```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
-}
-```
-
 ### Useful resources
 
 - [Freecodecamp](https://www.freecodecamp.org) 
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const screen = document.querySelector(".screen input");
 const del = document.getElementById("del");
 const reset = document.getElementById("reset");
 const equal = document.getElementById("equal");
+const scrollSec = document.querySelectorAll(".scroll-sec input");
+const toggle = document.querySelector(".btn");
 
 numberBtn.forEach((el) => {
     el.addEventListener("click", () => {
@@ -16,13 +18,14 @@ numberBtn.forEach((el) => {
 });
 
 del.addEventListener("click", () => {
-    if (screen.value == "Invalid Input") {
-        screen.value = '0'
+    if (screen.value == "Invalid Input" || screen.value == "Infinity") {
+        screen.value = "0";
+    } else if (screen.value == "0") {
+        screen.value = "0";
     } else {
         value = screen.value;
         screen.value = value.substring(0, value.length - 1);
     }
-    
 });
 
 reset.addEventListener("click", () => {
@@ -35,9 +38,8 @@ equal.addEventListener("click", () => {
         if (exp) {
             screen.value = eval(exp);
         }
-        console.log(screen.value);
     } catch (error) {
-        screen.value = "Invalid Input"
+        screen.value = "Invalid Input";
     }
 });
 
@@ -50,3 +52,56 @@ operation.forEach((el) => {
         }
     });
 });
+
+scrollSec.forEach((el) => {
+    el.addEventListener("click", () => {});
+});
+
+scrollSec.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.id == "one") {
+            setTheme("default");
+            toggle.style = `left: 5px`;
+        } else if (btn.id == "two") {
+            setTheme("light");
+            toggle.style = `left: 25px`;
+        } else {
+            setTheme("dark");
+            toggle.style = `left: 45px`;
+        }
+    });
+});
+
+let theme = localStorage.getItem("theme");
+
+let trackPos = 5 // track ball position
+themeOption = ['default', 'light', 'dark']
+
+for (let i = 0; i < themeOption.length; i++){
+
+    trackPos = 20 * i + 5
+
+    if (theme == themeOption[i]) {
+        toggle.style = `left: ${trackPos}px`
+    }
+}
+
+if (theme == null) {
+    setTheme("default");
+} else {
+    setTheme(theme);
+}
+
+function setTheme(mode) {
+    if (mode == "default") {
+        document.getElementById("theme-style").href = "/style.css";
+    }
+    if (mode == "light") {
+        document.getElementById("theme-style").href = "/lightTheme.css";
+    }
+    if (mode == "dark") {
+        document.getElementById("theme-style").href = "/darkTheme.css";
+    }
+
+    localStorage.setItem("theme", mode);
+}
